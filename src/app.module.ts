e8501318b0d1f0122aac69dacffecd5939badebd
Cauã R. Pereira <7aauac@gmail.com>
2025-01-17 10:07:52 -0300

    :sparkles: feat: Criado a entity de Produto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Produto } from './produto/entities/produto.entity';
+import { ProdutoModule } from './produto/produto.module';
 
 @Module({
   imports: [
@@ -10,9 +12,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'app_user',
       password: 'app_password',
       database: 'db_gamesstore',
-      entities: [],
+      entities: [Produto],
       synchronize: true,
     }),
+    ProdutoModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/produto/entities/produto.entity.ts
@@ -0,0 +1,36 @@
+import { Transform, TransformFnParams } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { NumericTransformer } from '../../util/numerictransformer';
+
+@Entity({ name: 'tb_produtos' })
+export class Produto {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Transform(({ value }: TransformFnParams) => value?.trim())
+  @IsNotEmpty()
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  titulo: string;
+
+  @IsNotEmpty()
+  @Column({ type: 'varchar', length: 1000, nullable: false })
+  descricao: string;
+
+  @Column({ type: 'date' })
+  data_lancamento: Date;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsNotEmpty()
+  @IsPositive()
+  @Column({
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+    transformer: new NumericTransformer(),
+  })
+  preco: number;
+
+  @Column({ type: 'varchar', length: 5000, nullable: true })
+  foto: string;
+}
