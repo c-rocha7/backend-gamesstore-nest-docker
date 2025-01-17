7ec36cdffea798a3f4cf3fdb7dd61e00ce9092db
Cauã R. Pereira <7aauac@gmail.com>
2025-01-17 15:01:15 -0300

    :sparkles: feat: Criado o método update

--- src/categoria/services/categoria.service.ts
+++ src/categoria/services/categoria.service.ts
@@ -41,4 +41,10 @@ export class CategoriaService {
   async create(categoria: Categoria): Promise<Categoria> {
     return await this.categoriaRepository.save(categoria);
   }
+
+  async update(categoria: Categoria): Promise<Categoria> {
+    await this.findById(categoria.id);
+
+    return await this.categoriaRepository.save(categoria);
+  }
 }
